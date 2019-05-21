9d7124c42b6446e03bc9b1195670b7f84a29dac4
Scott Pollack <skotep@gmail.com>
2019-05-21 13:31:55 -0700

    ifdef __cplusplus

--- src/streams/emscripten_browser.h
+++ src/streams/emscripten_browser.h
@@ -1,10 +1,14 @@
 #ifndef INCLUDE_streams_emscripten_browser_h__
 #define INCLUDE_streams_emscripten_browser_h__
 
+#ifdef __cplusplus
 extern "C" {
+#endif
 
 extern int git_open_emscripten_stream(git_stream **out, const char *host, const char *port);
 
+#ifdef __cplusplus
 }
+#endif
 
 #endif
