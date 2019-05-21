a9fedaa4972343e3dc5cd76f2960937a8d8dace6
Scott Pollack <scott@8thwall.com>
2019-05-21 13:26:32 -0700

    r0: make XHR requests async

--- src/streams/emscripten_browser.h
+++ src/streams/emscripten_browser.h
@@ -1,6 +1,10 @@
 #ifndef INCLUDE_streams_emscripten_browser_h__
 #define INCLUDE_streams_emscripten_browser_h__
 
+extern "C" {
+
 extern int git_open_emscripten_stream(git_stream **out, const char *host, const char *port);
+
+}
 
 #endif
